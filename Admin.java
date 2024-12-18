522c69bd3733c702be6c270ff6fe2e1360017a7a
Bestbatazaaa <116482632+Bestbatazaaa@users.noreply.github.com>
2024-12-18 16:48:59 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -3,15 +3,21 @@ import java.time.LocalDate;
 public class Admin extends User{
     private String role;
 
-    public Admin(String name, LocalDate dob) {
-        super(name,dob);
+    public Admin() {
+        super();
+        this.role = "Admin";
+    }
+
+    public Admin(String name, int year, int month, int date) {
+        super(name,year,month,date);
         this.role = "Admin";
     }
 
     public void displayinfo(boolean full) {
-        super.displayinfo(); 
+        super.displayinfo();
         if(full == true) {
             System.out.println("Role: " + role);
         }
     }
+
 }

ca1139b3f12ab58003ce87b4b552cc706ca9e255
Bestbatazaaa <116482632+Bestbatazaaa@users.noreply.github.com>
2024-12-18 16:47:32 +0700

    Update Lec05.java

--- Lec05.java
+++ Lec05.java
@@ -2,9 +2,9 @@ import java.time.LocalDate;
 
     public class Lec05 {
         public static void main(String[] args) throws Exception {
-            User john = new User("John", LocalDate.of(1954, 2, 18));
+            User john = new User("John", 1954, 2, 18);
             john.displayinfo();
-            Admin nicholas = new Admin("Nicholas", LocalDate.of(1964, 1, 7));
+            Admin nicholas = new Admin("Nicholas", 1964, 1, 7);
             nicholas.displayinfo(true);
             nicholas.displayinfo(false);
         }
